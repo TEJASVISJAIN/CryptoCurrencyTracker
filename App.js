b35b85d364c79eedb91cceb0d3090e79a79c69ef
TEJASVISJAIN <tejasvisjain0214@gmail.com>
2022-01-27 00:32:44 +0530

    feat: starter for cryptoTracker

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>App</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
